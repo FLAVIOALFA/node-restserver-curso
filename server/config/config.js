6d0ac7e6d394fe50879421bc3ca9c643fc802022
FLAVIOALFA <fg.alfaro94@gmail.com>
2018-04-28 23:53:42 -0300

    Agregando variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'dev') {
 
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:123123@ds261479.mlab.com:61479/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
